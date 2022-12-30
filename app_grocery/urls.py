d463207bfb0671b1e325bbe02127570893aa6875
JTWATTZ <thisisinsane4what@outlook.com>
2022-12-30 05:30:47 +0100

    Capstone update

--- app_grocery/urls.py
+++ app_grocery/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path("<str:user>", views.hungry_name, name="hungry_name"),#15/Dec/2022 this is a greeting
     path('about/', views.about, name = 'about'),
     path('recipe/', views.recipe, name = 'recipe'),#must work on getting this page
-    #to display the output
-    # path('home')
 ]
--- app_grocery/views.py
+++ app_grocery/views.py
@@ -8,80 +8,99 @@ apikey = api_key()
 
 def home(request):
     if request.method=='POST':
+        print('hello')
         params = {
     'instructionsRequired':True,
     'query':request.POST['dish'],
     'addChildren':True,
     'number':20,
-    #this is how you can take the input and put in your search.  'dish' is in home.html
-    #'cuisine':'italian',#there is a big list but will come back to
-    # 'intolerances':'gluten',#there is a big list but will come back to
-    # 'includeIngredients':'tomato,cheese',#have to comeback to or leave
-    # 'excludeIngredients':'eggs',#comeback to as well
-    'type':'main course', #will comeback to
-    
-    # 'addRecipeInformation':'true',
-    # 'addRecipeNutrition':'true',
-    # 'ignorePantry':'true',
-    # 'sort':'calories'
-}
+    'excludeIngredients':True,#comeback to as well
+    'addRecipeInformation':True,
+}#this is how you can take the input and put in your search.  'dish' is in home.html
 
         url = requests.get(f"https://api.spoonacular.com/recipes/complexSearch?apiKey={apikey}",params=params)
         recipe = url.json()
-        recipe_image = recipe['results'][0]['title'],recipe['results'][0]['image']
-        recipe_image_url = recipe_image[1]
+        recipe_image = [recipe['results'][0]['title'],recipe['results'][0]['image']]
+        recipe_image_url = recipe_image[1]##Dec28
         recipe_title = recipe_image[0]
-        # recipe_items = recipe_  # Need to get the actual items from the json.
-        print(recipe_image[1])
+        #print("picture of food",picture_of_food)
+        whats_at_home = requests.get(f"https://api.spoonacular.com/food/ingredients/search?apiKey={apikey}",params=params)
+        url = requests.get(f"https://api.spoonacular.com/recipes/complexSearch?apiKey={apikey}",params=params)##added 27Dec from test file
+
+        recipe = url.json()##added 27Dec from test file
+        info = recipe['results'][0]['id']##added 27Dec from test file
+        url2= requests.get(f"https://api.spoonacular.com/recipes/{info}/information?apiKey={apikey}")##added 27Dec from test file
+        recipe2 = url2.json()
+        recipe = url.json()
+        instructions=recipe2["instructions"] 
+# print(recipe['results'][0]['title'])##stopped 28 Dec
+# print(recipe['results'][0]['image'])##stopped 28 Dec
+        picture_of_food = requests.get(recipe['results'][0]['image'])
+        extend_ingred = recipe2["extendedIngredients"]
+        def recipe_list():  
+            items = []
+            for numz in range(0,len(extend_ingred)):
+
+                items.append(extend_ingred[numz]['name'])
+            return items
+
+        item_to_get=recipe_list()
 
         context = {
-            'recipe_image_url':recipe_image_url,
+            'recipe_image_url':recipe_image_url,##28Dec
             'recipe_title':recipe_title,
             'picture_of_food':picture_of_food,
+            'instructions':instructions,##added 27Dec from test file
+            'item_to_get':item_to_get,
 
             #recipe is the key and we are using it as the value. THis is what is in the home.html file and is listed in the {{}}
             # make a for loop for the results in the html file.
         }
-        return render(request, 'pages/home.html', context)# this is app_grocery/home
+        return render(request, 'pages/home.html',context)# this is app_grocery/home
 
     else:
             
         return render(request, 'pages/home.html')# this is app_grocery/home.  This is the GET method
+"""
+BIG CHANGE WHICH IS ME TRYING TO TAB EVERYTING AFTER LINE 46 OVER TO FIT IN THE HOME FUNCTION
 
+"""
+##Dec29 changed from about to home
 def about(request):
-    return render(request, 'pages/about.html')# this is app_grocery/about
-
-
-
+    return render(request, 'pages/home.html')# this is app_grocery/about
 #params is the request object and the = is the var
 params = {
-     'instructionsRequired':'true',
-     'query':'true'
-    # 'cuisine':'italian',#there is a big list but will come back to
-    # 'intolerances':'gluten',#there is a big list but will come back to
-    # 'includeIngredients':'tomato,cheese'#have to comeback to or leave
-    # 'excludeIngredients':'eggs',#comeback to as well
-    # 'type':'main course', #will comeback to
-    
-    #  'addRecipeInformation':'true',
-    # 'addRecipeNutrition':'true',
-    #  'ignorePantry':'true',
-    # 'sort':'calories'
+     'instructionsRequired':'True',
+     'query':'True',
+     'addRecipeInformation':True
  }
+##params.update({"query":True})##added 28 Dec
+
 whats_at_home = requests.get(f"https://api.spoonacular.com/food/ingredients/search?apiKey={apikey}",params=params)
-url = requests.get(f"https://api.spoonacular.com/recipes/complexSearch?apiKey={apikey}",params=params)
+url = requests.get(f"https://api.spoonacular.com/recipes/complexSearch?apiKey={apikey}",params=params)##added 27Dec from test file
+
+recipe = url.json()##added 27Dec from test file
+info = recipe['results'][0]['id']##added 27Dec from test file
+url2= requests.get(f"https://api.spoonacular.com/recipes/{info}/information?apiKey={apikey}")##added 27Dec from test file
+recipe2 = url2.json()
 recipe = url.json()
-print(recipe['results'][0]['title'])
-print(recipe['results'][0]['image'])
+instructions=recipe2["instructions"] 
+# print(recipe['results'][0]['title'])##stopped 28 Dec
+# print(recipe['results'][0]['image'])##stopped 28 Dec
 picture_of_food = requests.get(recipe['results'][0]['image'])
+
 def recipe(request):
-    return render(request, 'pages/about.html')
+    return render(request, 'pages/home.html')##Dec29 changed from about to home
 #Ask if I should remove this code and place it in html. 
-def hungry_name(request, user):
-    return HttpResponse(f"Hello hungry person.  Lets get your food choices, {user}")
-recipe1 = whats_at_home.json()   
-def recipe1(request):
-    return render(request, 'pages/about.html')
+
+# recipe1 = whats_at_home.json()   ##added 27Dec from test file
+# def recipe1(request):
+#     return render(request, 'pages/about.html')
+    ##Testing 29Dec
+
+   
+##params.update({"instructions":instructions})##This is what I need help with##Dec28
+
 
 # print(recipe1['results'][0]['image'])
 # def recipe1(request):
